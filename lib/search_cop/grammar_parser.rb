58a50c49c9d4fd9e011d6356359fcc8bc4b1e375
Tobias Wermuth <tobias.wermuth@wooga.net>
2015-05-26 11:17:12 +0200

    add 'in' operator parser

--- lib/search_cop/grammar_parser.rb
+++ lib/search_cop/grammar_parser.rb
@@ -8,14 +8,28 @@ module SearchCop
   class GrammarParser
     attr_reader :query_info
 
+    In_Operator_Regex = /(([a-zA-Z]*) in \(([a-zA-Z|, ]*)\))/
+
     def initialize(query_info)
       @query_info = query_info
     end
 
     def parse(string)
+      string = convert_in_operator_to_or_chain string
       node = SearchCopGrammarParser.new.parse(string) || raise(ParseError)
       node.query_info = query_info
       node.evaluate
+    end
+
+    def convert_in_operator_to_or_chain(string)
+      string.scan(In_Operator_Regex).each do |in_exp, param, list|
+          list.split(",").map(&:strip).map do |value|
+             "#{param} = #{value}"
+          end.join(" or ")
+
+          string = string.sub(in_exp, subquery)
+      end 
+      string
     end
   end
 end
