b1d5f275b927df024d8dbb569eccabf13a60673f
Tobias Wermuth <tobynextdoor@gmail.com>
2015-11-05 14:11:35 +0100

    add 'in' operator (parsed to or chain)

--- lib/search_cop/grammar_parser.rb
+++ lib/search_cop/grammar_parser.rb
@@ -13,9 +13,23 @@ module SearchCop
     end
 
     def parse(string)
+      string = parse_in_operator string
       node = SearchCopGrammarParser.new.parse(string) || raise(ParseError)
       node.query_info = query_info
       node.evaluate
+    end
+
+    def parse_in_operator(string)
+      in_operator_regex = /(([a-zA-Z_]*) in \(([a-zA-Z0-9_|, ]*)\))/
+      string.scan(in_operator_regex).each do |in_exp, param, list|
+        subquery = list.split(",").map(&:strip).map do |value|
+          "#{param} = #{value}"
+        end.join(" or ")
+
+        string = string.sub(in_exp, "(#{subquery})")
+      end
+
+      string
     end
   end
 end
